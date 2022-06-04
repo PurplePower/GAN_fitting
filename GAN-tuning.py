- fixed SWG gradient penalty problem - added some test data summaries

GAN-tuning.py
@@ -13,7 +13,7 @@ from utils.structures import *
 from visualizers.plots import plot_2d_density, plot_2d_discriminator_judge_area
 
 if __name__ == '__main__':
-    model_type = SWG
+    model_type = SWGAN
     save_path = f'pics/{model_type.__name__.lower()}'
     empty_directory(save_path)
     # x, sampler = make_cross_line_points(1024, k=3, path=save_path)
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     # dataset = tf.data.Dataset.from_tensor_slices(x)
 
     latent_factor = 5
-    D, G = level_4_structure(2, latent_factor)
+    D, G = level_3a_structure(2, latent_factor)
     gan = None
 
     if model_type is GAN:
@@ -86,16 +86,23 @@ if __name__ == '__main__':
             divergence='pearson-chi-square'
         )
     elif model_type is SWG:
+        # lr = PiecewiseConstantDecay([5000, 7000], [1e-4, 5e-5, 1e-5])
+        ipe = n_samples // 64  # scale lr
         gan = SWG(
             2, latent_factor, D=D, G=G,
             # d_optimizer=SGD(1e-0), g_optimizer=SGD(1e-0),
-            d_optimizer=Adam(1e-4), g_optimizer=Adam(1e-4),
-            use_discriminator=True, n_directions=1024,
+            # d_optimizer=Adam(1e-4), g_optimizer=Adam(1e-4),
+            d_optimizer=Adam(PiecewiseConstantDecay([5000 * ipe, 7000 * ipe], [1e-4, 5e-5, 1e-5])),
+            g_optimizer=Adam(PiecewiseConstantDecay([5000 * ipe, 7000 * ipe], [1e-4, 5e-5, 1e-5])),
+            use_discriminator=True, n_directions=128,
         )
     elif model_type is SWGAN:
+        ipe = n_samples // 64  # scale lr
         gan = SWGAN(
             2, latent_factor, D=D, G=G,
-            d_optimizer=Adam(1e-5), g_optimizer=Adam(1e-5),
+            # d_optimizer=Adam(1e-5), g_optimizer=Adam(1e-5),
+            d_optimizer=Adam(PiecewiseConstantDecay([5000 * ipe, 7000 * ipe], [1e-4, 5e-5, 1e-5])),
+            g_optimizer=Adam(PiecewiseConstantDecay([5000 * ipe, 7000 * ipe], [1e-4, 5e-5, 1e-5])),
             lambda1=1.0, lambda2=1.0
         )
     else:
@@ -110,10 +117,11 @@ if __name__ == '__main__':
             sampler=sampler, sample_number=512,
             metrics=[JSD()])
     else:
+        jsf = JudgeSurface(save_path + '/surfaces')
         losses, metrics = gan.train(
-            x, 5000, batch_size=64, sample_interval=sample_interval,
-            sampler=sampler, sample_number=512, dg_train_ratio=3,
-            metrics=[JSD(), JudgeSurface(save_path + '/surfaces')])
+            x, 10000, batch_size=64, sample_interval=sample_interval,
+            sampler=sampler, sample_number=512, dg_train_ratio=1,
+            metrics=[JSD(), jsf])
 
     # tf.profiler.experimental.stop()
     empty_directory(save_path + '/model')

models/SWG.py
@@ -93,7 +93,7 @@ class SWG(BaseGAN):
 
         with tf.GradientTape() as tape:
             tape.watch([interpolate])
-            d_interpolate_logits = self.discriminator(interpolate)
+            d_interpolate_logits = self.discriminator(interpolate)[1]
 
         grads = tape.gradient(d_interpolate_logits, interpolate)
         return tf.reduce_mean(tf.square(tf.norm(grads, axis=1) - 1.0))

models/SWGAN.py
@@ -19,7 +19,9 @@ class SWGAN(BaseGAN):
 
     Using orthogonal projection and update in Stiefel manifold.
 
+    !!!!!!!!
     [Currently Deprecated] unstable training.
+    Implementation may be buggy or wrong. See the implementation in the paper.
 
     """
 
@@ -50,6 +52,7 @@ class SWGAN(BaseGAN):
         self.swd_blocks = [
             tf.Variable(
                 np.linalg.svd(np.random.normal(0, 1, (self.feat_dim, self.feat_dim)))[0],
+                # np.random.normal(0, 1, (self.feat_dim, self.feat_dim)),
                 dtype=tf.float32,
                 name=f'{self._swd_name_pfx}{i}'
             )
@@ -84,12 +87,14 @@ class SWGAN(BaseGAN):
             x_fake = generator(z, training=True)
 
             # encode to features
+            # shape is n_samples, n_feat
             real_feat = discriminator(x_real, training=True)
             fake_feat = discriminator(x_fake, training=True)
 
             # SWD blocks
-            disc_real = tf.reduce_mean(self._swd_blocks_compute(real_feat), axis=1)
-            disc_fake = tf.reduce_mean(self._swd_blocks_compute(fake_feat), axis=1)
+            # average over n_samples
+            disc_real = tf.reduce_mean(self._swd_blocks_compute(real_feat), axis=0)
+            disc_fake = tf.reduce_mean(self._swd_blocks_compute(fake_feat), axis=0)
 
             # losses
             disc_loss = tf.reduce_mean(disc_real) - tf.reduce_mean(disc_fake)
@@ -153,17 +158,14 @@ class SWGAN(BaseGAN):
 
     @tf.function
     def _gradient_penalty_2(self, real_feat, fake_feat):
-        t = tf.random.uniform([real_feat.shape[0], 1])
-
         with tf.GradientTape() as tape:
+            t = tf.random.uniform([real_feat.shape[0], 1])
             interpolated = real_feat * t + fake_feat * (1 - t)
             tape.watch([interpolated])
-            activated = self._swd_blocks_compute(interpolated)
+            activated = tf.reduce_mean(self._swd_blocks_compute(interpolated))
 
         grad = tape.gradient(activated, interpolated)
         return tf.reduce_mean(tf.square(grad - 0.001))
-
-
 
     def train(
             self, dataset, epochs, batch_size=64,

utils/picdist.py
@@ -11,7 +11,7 @@ def image2dots(
     """
 
     # to gray scale and make binary
-    img = ImageOps.flip(img)    # upside down
+    img = ImageOps.flip(img)  # upside down
     pixels = np.array(img.convert('L'))
     pixels = pixels <= 127  # black pixels converted to 1
     points = np.argwhere(pixels)
@@ -26,12 +26,15 @@ def image2dots(
     points[:, [0, 1]] = points[:, [1, 0]]  # swap columns
 
     # round to nearest n samples
-    n_multiples = n_samples // points.shape[0]
-    if n_samples - (n_multiples * points.shape[0]) > \
-            (n_multiples + 1) * points.shape[0] - n_samples:
-        n_multiples += 1
+    if n_samples >= points.shape[0]:
+        n_multiples = n_samples // points.shape[0]
+        if n_samples - (n_multiples * points.shape[0]) > \
+                (n_multiples + 1) * points.shape[0] - n_samples:
+            n_multiples += 1
 
-    points = np.tile(points, [n_multiples, 1])  # stack at axis 0
+        points = np.tile(points, [n_multiples, 1])  # stack at axis 0
+    else:
+        pass  # keep points as it is
 
     # add noise
     points = points + np.random.normal(0, scale=std, size=points.shape)